e3466674876c73ce3ca7cf667762ba3fe3a2c0d2
albe2 <alberic.flour-de-saegher@epitech.eu>
2024-11-22 12:20:30 +0100

    ADD: home page style rework

--- EpiAirConsole/src/routes/+page.svelte
+++ EpiAirConsole/src/routes/+page.svelte
@@ -8,12 +8,18 @@
 
 
 <div class="flex w-full h-full justify-center items-center">
-    <div class="flex flex-col w-1/2 h-1/5 justify-between items-center">
-        <h1> Playing Games </h1>
-        <p> with ur freinds :) </p>
+    <video autoplay loop muted class="absolute w-auto min-w-full min-h-full max-w-none brightness-75">
+        <source src="/aqua.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+    </video>
+    <div class="flex flex-col z-10 w-1/2 h-1/5 justify-between items-center mb-4">
+        <div class="flex flex-col w-full h-2/3 justify-evenly items-center">
+            <h1 class="text-4xl"> Play Games </h1>
+            <p class="font-bold text-3xl"> With your friends </p>
+        </div>
         <button
-                class="bg-emerald-600 rounded w-1/5"
+                class="bg-black rounded w-1/2 h-1/3 font-bold text-lg"
                 on:click={() => goto(`/${randomNumber}`)}
-        > Play </button>
+        > Start Playing Now </button>
     </div>
 </div>
