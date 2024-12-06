effb3f68d5e76741ae7f0df605b60b9385aab5cc
albe2 <alberic.flour-de-saegher@epitech.eu>
2024-12-06 11:01:09 +0100

    FIX: vercel error delete fix

--- EpiAirConsole/src/routes/+layout.svelte
+++ EpiAirConsole/src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
 	import '../app.postcss';
 
-	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
-    document.cookie = "__vercel_live_token=your_token; SameSite=None; Secure";
-
 </script>
 
 <div class="w-full h-full">
